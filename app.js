fa4d65531d8bdd89cd4c39b589205bfdbe938ebb
MatthewJOwens <mjowens@mail.com>
2020-06-11 12:08:46 -0600

    tetrominos move down and stop

--- app.js
+++ app.js
@@ -51,4 +51,83 @@ document.addEventListener('DOMContentLoaded', () => {
     [width, width + 1, width + 2, width + 3]
   ]
 
+  const theTetrominoes = [lTetromino, rTetromino, zTetromino, rzTetromino, tTetromino, oTetromino, iTetromino]
+
+  let currentPosition = 4
+  let currentRotation = 0
+
+  //randomly select a tetromino and its first rotation
+  let random = Math.floor(Math.random() * theTetrominoes.length)
+  let current = theTetrominoes[random][currentRotation]
+
+  //draw the tetromino
+  function draw() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.add('tetromino')
+    })
+  }
+
+  //undraw the Tetromino
+  function undraw() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.remove('tetromino')
+    })
+  }
+
+  //make the tetromino move down every second
+  timerId = setInterval(moveDown, 1000)
+
+  //move down function 
+  function moveDown() {
+    undraw()
+    currentPosition += width
+    draw()
+    freeze()
+  }
+
+  //freeze function
+  function freeze() {
+    if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+      current.forEach(index => squares[currentPosition + index + width].classList.add("taken"))
+      //start a new tetromino falling
+      random = Math.floor(Math.random() * theTetrominoes.length)
+      current = theTetrominoes[random][currentRotation]
+      currentPosition = 4
+      draw()
+    }
+  }
+
+  //move the tetromino left, unless it is at the edge or there is a blockage 
+  function moveLeft() {
+    undraw()
+    const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+
+    if (!isAtLeftEdge) {
+      currentPosition -= 1
+    }
+    if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition += 1
+    }
+
+    draw()
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
