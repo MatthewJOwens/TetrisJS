b122cf23b16fc7724e13a231d3d1a16900d675b2
MatthewJOwens <mjowens@mail.com>
2020-06-11 12:57:34 -0600

    rotate works, corrected moveRight

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const ScoreDisplay = document.querySelector('#score')
   const StartBtn = document.querySelector('#start-button')
   const width = 10
+  let nextRandom = 0
 
   //The Tetrominoes
   const lTetromino = [
@@ -82,7 +83,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (e.keyCode === 37) {
       moveLeft()
     } else if (e.keyCode === 38) {
-      //rotate
+      rotate()
     } else if (e.keyCode === 39) {
       moveRight()
     } else if (e.keyCode === 40) {
@@ -104,10 +105,12 @@ document.addEventListener('DOMContentLoaded', () => {
     if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
       current.forEach(index => squares[currentPosition + index].classList.add("taken"))
       //start a new tetromino falling
-      random = Math.floor(Math.random() * theTetrominoes.length)
+      random = nextRandom
+      nextRandom = Math.floor(Math.random() * theTetrominoes.length)
       current = theTetrominoes[random][currentRotation]
       currentPosition = 4
       draw()
+      displayShape()
     }
   }
 
@@ -129,7 +132,7 @@ document.addEventListener('DOMContentLoaded', () => {
   //move the tetromino right, unless it is at the edge or there is a blockage
   function moveRight() {
     undraw()
-    const isAtRightEdge = current.some(index => squares[currentPosition + index] % width === width - 1)
+    const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1)
 
     if (!isAtRightEdge) currentPosition += 1
 
@@ -139,12 +142,44 @@ document.addEventListener('DOMContentLoaded', () => {
     draw()
   }
 
+  //rotate the tetromino
+  function rotate() {
+    undraw()
+    currentRotation++
+    if (currentRotation === current.length) { //if the current rotation gets to 4, make it go back to 0
+      currentRotation = 0
+    }
+    current = theTetrominoes[random][currentRotation]
+    draw()
+  }
 
 
+  //show next tetromino in mini-grid
+  const displaySquares = document.querySelectorAll('.mini-grid div')
+  const displayWidth = 4
+  let displayIndex = 0
 
+  //the Tetrominos w/o rotations
+  const upNextTetrominoes = [
+    [1, displayWidth + 1, displayWidth * 2 + 1, 2], //lTetromino
+    [0, 1, displayWidth + 1, displayWidth * 2 + 1], //rTetromino
+    [0, displayWidth, displayWidth + 1, displayWidth * 2 + 1], //zTetromino
+    [1, displayWidth, displayWidth + 1, displayWidth * 2], //rzTetromino
+    [displayWidth, displayWidth + 1, displayWidth + 2, displayWidth * 2 + 1], //tTetromino
+    [0, 1, displayWidth, displayWidth + 1], //oTetromino
+    [1, displayWidth + 1, displayWidth * 2 + 1, displayWidth * 3 + 1], //iTetromino
+  ]
 
-
-
+  //display the shape in the mini-grid
+  function displayShape() {
+    //remove any trace of a tetromino from the entire grid
+    displaySquares.forEach(square => {
+      square.classList.remove('tetromino')
+    })
+    upNextTetrominoes[nextRandom].forEach(index => {
+      displaySquares[displayIndex + index].classList.add('tetromino')
+    })
+  }
 
 
 
