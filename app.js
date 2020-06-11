82e654bc365213dc6da1408e679bd724e0844bb1
MatthewJOwens <mjowens@mail.com>
2020-06-10 22:15:46 -0600

    Tetromino patterns

--- app.js
+++ app.js
@@ -5,6 +5,50 @@ document.addEventListener('DOMContentLoaded', () => {
   const StartBtn = document.querySelector('#start-button')
   const width = 10
 
-
+  //The Tetrominoes
+  const lTetromino = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2]
+  ]
+  const rTetromino = [
+    [0, 1, width + 1, width * 2 + 1],
+    [2, width, width + 1, width + 2],
+    [1, width + 1, width * 2 + 1, width * 2 + 2],
+    [width, width + 1, width + 2, width * 2]
+  ]
+  const zTetromino = [
+    // [1, width + 1, width + 2, width * 2 + 2],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    // [1, 2, width, width + 1]
+  ];
+  const rzTetromino = [
+    [1, width, width + 1, width * 2],
+    [0, 1, width + 1, width + 2],
+    [1, width, width + 1, width * 2],
+    [0, 1, width + 1, width + 2]
+  ]
+  const tTetromino = [
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1],
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1]
+  ];
+  const oTetromino = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1]
+  ]
+  const iTetromino = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3]
+  ]
 
 })
